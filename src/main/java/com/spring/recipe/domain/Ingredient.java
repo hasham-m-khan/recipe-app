cb21c8efd240d242da135199f1e9995fb4dc0e08
Hasham M Khan <hasham.m.khan@outlook.com>
2025-06-06 01:26:42 -0500

    Creates unit of measure and establishes one-to-one with ingredient.

--- src/main/java/com/spring/recipe/domain/Ingredient.java
+++ src/main/java/com/spring/recipe/domain/Ingredient.java
@@ -17,9 +17,8 @@ public class Ingredient {
     @ManyToOne
     private Recipe recipe;
 
-    //TODO:
-    //private UnitOfMeasure uom;
-
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
 
     public Long getId() {
         return id;
@@ -51,5 +50,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
